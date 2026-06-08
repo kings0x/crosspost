9996cc2af7880ffaca1776850e8fc338afbdc317
Kingsley <agukingsley450@gmail.com>
2026-06-08 12:27:58 +0100

    fix: fix: solify oauth_states table

--- migrations/000005_create_oauth_states_table.up.sql
+++ migrations/000005_create_oauth_states_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE oauth_states(
     states          UUID PRIMARY KEY,
     user_id         UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    platform        TEXT,
-    intent          TEXT,
-    created_at      TIMESTAMPTZ,
+    platform        TEXT NOT NULL,
+    intent          TEXT NOT NULL,
+    created_at      TIMESTAMPTZ NOT NULL DEFAULT now(),
     expires_at      TIMESTAMPTZ
 )
