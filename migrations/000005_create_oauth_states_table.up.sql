8673a6a1a07f3586d42f366ac867f18febf7fd7c
Kingsley <agukingsley450@gmail.com>
2026-06-08 12:24:38 +0100

    fix: fixed the oauth_states table

--- migrations/000005_create_oauth_states_table.up.sql
+++ migrations/000005_create_oauth_states_table.up.sql
@@ -1,8 +1,8 @@
-CREATE TABLE oauth_states{
+CREATE TABLE oauth_states(
     states          UUID PRIMARY KEY,
     user_id         UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     platform        TEXT,
     intent          TEXT,
     created_at      TIMESTAMPTZ,
     expires_at      TIMESTAMPTZ
-}
+)
